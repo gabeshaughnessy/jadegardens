11c69528d66cc8a3bb416cffce61aea2f8e9a57d
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-06-14 17:17:50 -0700

    acf updates

--- wp-content/plugins/jade-garden-products/lib/advanced-custom-fields/register_fields.php
+++ wp-content/plugins/jade-garden-products/lib/advanced-custom-fields/register_fields.php
@@ -2,6 +2,30 @@
 if(function_exists("register_field_group"))
 {
 	register_field_group(array (
+		'id' => 'acf_options-page',
+		'title' => 'options page',
+		'fields' => array (
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+	register_field_group(array (
 		'id' => 'acf_products',
 		'title' => 'Products',
 		'fields' => array (
